20a7c6da50408b5db9fd0994966292a7ec37bd49
mbernhardt6 <mbernhardt6@gmail.com>
2015-02-28 01:04:42 -0800

    Remove debug line
    No longer needed debug line removed.

--- tweeter.py
+++ tweeter.py
@@ -4,4 +4,3 @@ from tweeter_auth import *
 def SendTweet(message):
   twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
   twitter.update_status(status=message)
-  print(message)
