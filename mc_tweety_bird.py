d647343dec39146a8f5b7fe3bd65c07a574bbd43
mbernhardt6 <mbernhardt6@gmail.com>
2015-03-07 17:09:47 -0800

    Multiprocessing Rewrite
    Major change to script. (Oh god, what have I done?)
    
    - Will now spin up multiple processes to handle different log files.
    - Added some whitespace
    - Renamed some variables
    - Broke functions into separate library file (Because I hate myself?)
    - Added AllDeaths.log file from LogAll plugin
    
    TODO: Consolidate logs into single function that shards itself where
    needed. Lots of duplicated code.

--- mc_tweety_bird.py
+++ mc_tweety_bird.py
@@ -3,26 +3,28 @@
 
 # Python modules
 from signal import signal, SIGTERM
-import atexit
 import argparse
-import os
+import atexit
+import multiprocessing
 import sys
 import time
 # Homegrown modules
-import filer
 import logger
 import tweeter
+import tweety_libs
 
 # Variables
 log = "/var/log/mc_tweety_bird.log"
-mc_log = "/var/games/minecraft/servers/one/logs/latest.log"
+latest_log = "/var/games/minecraft/servers/one/logs/latest.log"
+death_log = "/var/games/minecraft/servers/one/plugins/LogAll/AllDeaths.log"
 base_folder = "/home/mc/python/"
-msg_queue_file = base_folder + "msg_queue"
-death_messages_file = base_folder + "death_messages.txt"
-seen_messages_file = base_folder + "seen_messages"
+pid_base = "/tmp/tweetybird"
+msg_queue_file_name = base_folder + "msg_queue"
+death_messages_file_name = base_folder + "death_messages.txt"
+seen_messages_base = base_folder + "seen_messages"
 # Number of death messages to keep in state
 # Should hold more than you would expect to happen on a single log iteration
-tweet_history = 1000
+kept_history = 1000
 # Number of tweets to send during each pass
 tweet_volume = 1
 # Time in seconds between log read resets
@@ -43,107 +45,149 @@ parser.add_argument('--tweet_messages',
 args = parser.parse_args()
 
 
-def StringInSet(test_string, test_set):
-  """Check if a string contains any string from a set of strings.
-
-  Args:
-    test_string: String to check against.
-    test_set: Set to test against.
-
-  Returns:
-    Boolean if one of set is found or not.
-  """
-  for item in test_set:
-    if item in test_string:
-      return True
-  return False
-
-
-def ReadFileData(target_file_name):
-  """Read data from a file ignoring commented out lines.
-
-  Args:
-    target_file_name: Filename of file to read.
-
-  Returns:
-    Contents of file as a set.
-  """
-  file_contents = set()
-  try:
-    target_file = open(target_file_name, 'r')
-    for line in target_file:
-      if line.encode('utf-8')[:1] != '#':
-        file_contents.add(line.encode('utf-8').replace('\n', ''))
-    target_file.close()
-  except:
-    logger.logMessage(log, "WARNING: Unspecified error processing %s" %
-        target_file_name)
-  return file_contents
-
-
-def WriteFileData(target_file_name, file_contents, line_count):
-  """Write data to a file.
-
-  Args:
-    file_contents: Content to be written to file as set.
-    target_file_name: Filename of file to write.
-    line_count: Number of files at end of file to retain. 0 skips trimming file.
-  """
-  target_file = open(target_file_name, 'w')
-  for line in file_contents:
-    target_file.write(line + '\n')
-  target_file.close()
-  if line_count > 0:
-    filer.tailFile(target_file_name, line_count)
-
-
-def ReadDeathMessagesFromLog(log_file_name, queue_file_name,
-    death_messages_file, seen_messages_file):
+def ReadMessagesFromLog(watch_file_name, queue_file_name,
+    death_messages_file_name, seen_messages_base):
   """Read Death Messages from MC Log and write out to queue file.
 
   Args:
-    log_file_name: File name to read messages from.
+    watch_file_name: File name to read messages from.
     queue_file_name: File name to write messages to for later tweeting.
-    death_messages: Set of messages to look for.
-    seen_messages: Set of messages to filter against to avoid repeats.
+    death_messages_file_name: Set of messages to look for.
+    seen_messages_base: Set of messages to filter against to avoid repeats.
   """
+  thread_name = "latest"
+  # Setup signal interrupt
+  signal(SIGTERM, lambda signum, stack_frame: exit(1))
+
+  # Verify script is not already running
+  # SetPid will exit if thread is already running
+  pid_file = tweety_libs.SetPid(thread_name, pid_base, log)
+  logger.logMessage(log, "%s: Starting thread." % thread_name)
+
   # Read filter and state data
-  death_messages = ReadFileData(death_messages_file)
-  seen_messages = ReadFileData(seen_messages_file)
+  death_messages = tweety_libs.ReadFileData(death_messages_file_name, log)
+  seen_messages_file_name = seen_messages_base + "_" + thread_name
+  seen_messages = tweety_libs.ReadFileData(seen_messages_file_name, log)
+
+  # Set cleanup
+  atexit.register(tweety_libs.Cleanup, thread_name, pid_file, log)
+  atexit.register(tweety_libs.WriteFileData, seen_messages_file_name,
+      sorted(seen_messages), kept_history)
+
   # Open log file for reading
-  log_file = open(log_file_name, 'r')
+  watch_file = open(watch_file_name, 'r')
   start_time = time.time()
   read_count = 0
+
+  # Start listening
   try:
     while 1:
-      where = log_file.tell()
-      line = log_file.readline()
+      where = watch_file.tell()
+      line = watch_file.readline()
       if not line:
         time.sleep(1)
-        log_file.seek(where)
+        watch_file.seek(where)
       else:
         read_count += 1
-        if (not StringInSet(line, seen_messages)
-            and StringInSet(line, death_messages)):
+        if (not tweety_libs.StringInSet(line, seen_messages)
+            and tweety_libs.StringInSet(line, death_messages)):
           with open(queue_file_name, 'a') as message_queue:
-            message_queue.write(line)
+            message_queue.write(line.split('[Server thread/INFO]:')[1].strip() +
+                "\n")
           seen_messages.add(line)
-          logger.logMessage(log, "Message submitted to tweet queue.")
+          logger.logMessage(log, "%s: Message submitted to tweet queue." %
+              thread_name)
       # Reset read position every hour to catch log rollover
       if ((time.time() - start_time) > reset_time):
-        logger.logMessage(log, "Reseting log file location. %s line(s) read." %
-            read_count)
-        log_file.close()
-        log_file = open(log_file_name, 'r')
-        log_file.seek(0, 0)
+        logger.logMessage(log, "%s: Reseting log file location. %s lines read."
+            % (thread_name, read_count))
+        watch_file.close()
+        watch_file = open(watch_file_name, 'r')
+        watch_file.seek(0, 0)
         start_time = time.time()
         read_count = 0
-        WriteFileData(seen_messages_file, sorted(seen_messages), tweet_history)
-  except:
-    logger.logMessage(log, "WARNING: Error detected while reading messages.")
+        tweety_libs.WriteFileData(seen_messages_file_name,
+            sorted(seen_messages), kept_history)
+  except Exception as e:
+    logger.logMessage(log, "WARNING: %s: %s" % (thread_name, e))
+
   # Write state data and close log file on exit
-  WriteFileData(seen_messages_file, sorted(seen_messages), tweet_history)
-  log_file.close()
+  logger.logMessage(log, "%s: Cleaning up on completion." % thread_name)
+  tweety_libs.WriteFileData(seen_messages_file_name, sorted(seen_messages),
+      kept_history)
+  watch_file.close()
+  tweety_libs.Cleanup(thread_name, pid_file, log)
+
+
+def ReadDeathMessageLog(watch_file_name, queue_file_name, seen_messages_base):
+  """Read Messages from AllDeath.log from LogAll plugin.
+
+  Args:
+    watch_file_name: File name to read messages from.
+    queue_file_name: File name to write messages to for later tweeting.
+    seen_messages_base: Set of messages to filter against to avoid repeats.
+  """
+  thread_name = "alldeaths"
+  # Setup signal interrupt
+  signal(SIGTERM, lambda signum, stack_frame: exit(1))
+
+  # Verify script is not already running
+  # SetPid will exit if thread is already running
+  pid_file = tweety_libs.SetPid(thread_name, pid_base, log)
+  logger.logMessage(log, "%s: Starting thread." % thread_name)
+
+  # Read state data
+  seen_messages_file_name = seen_messages_base + "_" + thread_name
+  seen_messages = tweety_libs.ReadFileData(seen_messages_file_name, log)
+
+  # Set cleanup
+  atexit.register(tweety_libs.Cleanup, thread_name, pid_file, log)
+  atexit.register(tweety_libs.WriteFileData, seen_messages_file_name,
+      sorted(seen_messages), kept_history)
+
+  # Open log file for reading
+  watch_file = open(watch_file_name, 'r')
+  start_time = time.time()
+  read_count = 0
+
+  # Start listening
+  try:
+    while 1:
+      where = watch_file.tell()
+      line = watch_file.readline()
+      if not line:
+        time.sleep(1)
+        watch_file.seek(where)
+      else:
+        read_count += 1
+        if not tweety_libs.StringInSet(line, seen_messages):
+          # Validate AllDeaths Message
+          if line.split(" ")[2].strip() != line.split(" ")[4].strip():
+            with open(queue_file_name, 'a') as message_queue:
+              message_queue.write(line.split(":")[3].strip() + "\n")
+            seen_messages.add(line)
+            logger.logMessage(log, "%s: Message submitted to tweet queue." %
+                thread_name)
+      # Reset read position every hour to catch log rollover
+      if ((time.time() - start_time) > reset_time):
+        logger.logMessage(log, "%s: Reseting log file location. %s lines read."
+            % (thread_name, read_count))
+        watch_file.close()
+        watch_file = open(watch_file_name, 'r')
+        watch_file.seek(0, 0)
+        start_time = time.time()
+        read_count = 0
+        tweety_libs.WriteFileData(seen_messages_file_name,
+            sorted(seen_messages), kept_history)
+  except Exception as e:
+    logger.logMessage(log, "WARNING: %s: %s" % (thread_name, e))
+
+  # Write state data and close log file on exit
+  logger.logMessage(log, "%s: Cleaning up on completion." % thread_name)
+  tweety_libs.WriteFileData(seen_messages_file_name, sorted(seen_messages),
+      kept_history)
+  watch_file.close()
+  tweety_libs.Cleanup(thread_name, pid_file, log)
 
 
 def TweetDeathMessages(queue_file_name, num_tweets):
@@ -153,53 +197,33 @@ def TweetDeathMessages(queue_file_name, num_tweets):
     queue_file_name: File to pull tweets from.
     num_tweets: Number of tweets to send in a single instance.
   """
-  tweet_list = sorted(ReadFileData(queue_file_name))
+  tweet_list = sorted(tweety_libs.ReadFileData(queue_file_name, log))
   for x in range(0, num_tweets):
     try:
       # Read message and remove from queue
-      raw_message = tweet_list.pop(0)
-      # Format message for tweet
-      message = raw_message.split('[Server thread/INFO]:')[1].strip()
+      message = tweet_list.pop(0)
       tweeter.SendTweet(message)
       logger.logMessage(log, "Update sent to Tweeter.")
     except:
       logger.logMessage(log, "Unable to find message to tweet.")
-  WriteFileData(queue_file_name, tweet_list, 0)
-
-
-def Cleanup():
-  """pid file cleanup for abnormal script termination.
-
-  Called with no arguments, removes pid file when script is abnormally
-  terminated.
-  """
-  logger.logMessage(log, "WARNING: Abnormal termination detected.")
-  try:
-    os.unlink(pidfile)
-    logger.logMessage(log, "pid file successfully removed.")
-  except:
-    logger.logMessage(log, "WARNING: Unable to remove pid file.")
+  tweety_libs.WriteFileData(queue_file_name, tweet_list, 0)
 
 
 if __name__ == "__main__":
-  # Setup signal interrupt
-  signal(SIGTERM, lambda signum, stack_frame: exit(1))
   if args.read_messages:
     # read_messages code path
-    # Set pid details
-    pid = str(os.getpid())
-    pidfile = "/tmp/tweetybird.pid"
-    # Verify script is not already running
-    if os.path.isfile(pidfile):
-      logger.logMessage(log, "%s exists, exiting." % pidfile)
-    else:
-      # Set pid file
-      file(pidfile, 'w').write(pid)
-      # Set cleanup for abnormal termination
-      atexit.register(Cleanup)
-      logger.logMessage(log, "Reading messages from log to queue.")
-      ReadDeathMessagesFromLog(mc_log, msg_queue_file, death_messages_file,
-          seen_messages_file)
+    # Setup threads
+    p_latest = multiprocessing.Process(target=ReadMessagesFromLog,
+        args=(latest_log, msg_queue_file_name, death_messages_file_name,
+        seen_messages_base))
+    p_alldeaths = multiprocessing.Process(target=ReadDeathMessageLog,
+        args=(death_log, msg_queue_file_name, seen_messages_base))
+
+    # Start threads
+    p_latest.start()
+    p_alldeaths.start()
   if args.tweet_messages:
     # tweet_messages code path
-    TweetDeathMessages(msg_queue_file, tweet_volume)
+    TweetDeathMessages(msg_queue_file_name, tweet_volume)
+  if not args.read_messages and not args.tweet_messages:
+    print "No flags declared."
---
+++ tweety_libs.py
@@ -0,0 +1,99 @@
+# Python modules
+import os
+import sys
+# Homegrown modules
+import filer
+import logger
+
+
+def StringInSet(test_string, test_set):
+  """Check if a string contains any string from a set of strings.
+
+  Args:
+    test_string: String to check against.
+    test_set: Set to test against.
+
+  Returns:
+    Boolean if one of set is found or not.
+  """
+  for item in test_set:
+    if item in test_string:
+      return True
+  return False
+
+
+def ReadFileData(target_file_name, log):
+  """Read data from a file ignoring commented out lines.
+
+  Args:
+    target_file_name: Filename of file to read.
+
+  Returns:
+    Contents of file as a set.
+  """
+  file_contents = set()
+  try:
+    target_file = open(target_file_name, 'r')
+    for line in target_file:
+      if line.encode('utf-8')[:1] != '#':
+        file_contents.add(line.encode('utf-8').replace('\n', ''))
+    target_file.close()
+  except:
+    logger.logMessage(log, "WARNING: Unspecified error processing %s" %
+        target_file_name)
+  return file_contents
+
+
+def WriteFileData(target_file_name, file_contents, line_count):
+  """Write data to a file.
+
+  Args:
+    file_contents: Content to be written to file as set.
+    target_file_name: Filename of file to write.
+    line_count: Number of files at end of file to retain. 0 skips trimming file.
+  """
+  target_file = open(target_file_name, 'w')
+  for line in file_contents:
+    target_file.write(line + '\n')
+  target_file.close()
+  if line_count > 0:
+    filer.tailFile(target_file_name, line_count)
+
+
+def SetPid(thread_name, pid_base, log):
+  """Function to handle cehcking and setting pid files.
+
+  If pid file doesn't exist this function will set it and return the path to the
+  parent script. If the pid file already exists this function will exit out of
+  the script.
+
+  Args:
+    thread_name: Identifier for each thread/function.
+
+  Returns:
+    path of pid file.
+  """
+  pid = str(os.getpid())
+  pid_file = pid_base + "_" + thread_name + ".pid"
+  if os.path.isfile(pid_file):
+    logger.logMessage(log, "%s exists, exiting." % pid_file)
+    sys.exit()
+  else:
+    file(pid_file, 'w').write(pid)
+    return pid_file
+
+
+def Cleanup(thread_name, pid_file, log):
+  """pid file cleanup for abnormal script termination.
+
+  Args:
+    thread_name: Human readable name for thread being cleaned up.
+    pid_file: File to check during cleanup.
+  """
+  logger.logMessage(log, "%s: Starting cleanup." % thread_name)
+  try:
+    os.unlink(pid_file)
+    logger.logMessage(log, "%s pid file successfully removed." % thread_name)
+  except:
+    logger.logMessage(log, "WARNING: Unable to remove %s pid file." %
+        thread_name)
